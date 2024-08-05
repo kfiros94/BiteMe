a0d522543b08645d496879984f808eb3f149e81a
Kfir Amoyal <kfiramoyal@gmail.com>
2024-08-05 12:31:03 +0300

    Fixing KfirBranch

--- BiteMe-Server/src/mainApp/ServerUIPro.java
+++ BiteMe-Server/src/mainApp/ServerUIPro.java
@@ -7,16 +7,14 @@ import logic.Order;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Vector;
 import guiPro.ServerPortFrameControllerPro;
-import server.DBController;
 import server.EchoServerPro;
 
 public class ServerUIPro extends Application {
 	final public static int DEFAULT_PORT = 5555;
-	public static DBController dbc;
-
+	
 	 
 	public static void main( String args[] ) throws Exception
-	  {   
+	   {   
 		 launch(args);
 	  } // end main
 	
@@ -31,42 +29,6 @@ public class ServerUIPro extends Application {
 	      }
 
 	   }
-	
-	public static void runServer(String p)
-	{
-		int port = 0;
-		try
-        {
-        	port = DEFAULT_PORT; //Set port to 5555
-          
-        }
-        catch(Throwable t)
-        {
-        	System.out.println("ERROR - Could not connect!");
-        }
-        dbc = new DBController(port);
-        
-        try
-        {
-        	dbc.listen();//start listening for connections
-        }
-        catch (Exception ex) 
-        {
-          System.out.println("ERROR - Could not listen for clients!");
-        }
-	}
-	public static void closeServer()
-	{
-		try 
-        {
-			dbc.sendToAllClients("Server Offline");
-            dbc.close(); //stop listenning for connections
-        } 
-        catch (Exception ex) 
-        {
-          System.out.println("ERROR - Could not stop listen for clients!");
-        }
-	}
 
 
 }
